9eca526ab93cfacd1fd07a279f58e509db5522d3
LytvynVitalii <v.litvin@dodotap.com>
2022-08-25 15:24:24 +0300

    Key change

--- src/API/GitHub.ts
+++ src/API/GitHub.ts
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.github.com/";
-const TOKEN = "Bearer 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";
+const TOKEN = "Bearer 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";
 
 async function searchUsers(name: string) {
   return await fetch(BASE_URL + `search/users?q=${name}&sort=repositories&per_page=100`, {
